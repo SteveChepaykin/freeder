bcba72f5917d7fc788bafc83d886e12cba50e229
LeFellowHuman <highgroundgamedev@gmail.com>
2022-04-22 23:16:24 +0300

    new: text selection of cur pos

--- lib/screens/edit_screen.dart
+++ lib/screens/edit_screen.dart
@@ -14,17 +14,37 @@ class _EditScreenState extends State<EditScreen> {
   final TextEditingController textedit = TextEditingController();
   final TextEditingController positionedit = TextEditingController();
   // final TextEditingController headeredit = TextEditingController();
+  List<String> textlist = [];
+  int index = 0;
+  String alltext = '';
+  int untillength = 0;
 
   @override
   void initState() {
-    textedit.text = widget.st.wholetext;
-    positionedit.text = (widget.st.lastindex + 1).toString();
+    // textedit.text = widget.st.wholetext;
+    // positionedit.text = (widget.st.lastindex + 1).toString();
+    index = widget.st.lastindex;
+    alltext = widget.st.wholetext;
     // headeredit.text = widget.st.title;
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
+    untillength = 0;
+    textlist = alltext.split(' ');
+    for (String a in textlist.sublist(0, index)) {
+      untillength += a.length;
+    }
+    untillength += index;
+    // print(untillength);
+    // textlist.insert(index, '█');
+    // .split(' ').join(
+    //       textlist.sublist(index + 1).toString(),
+    //     );
+    // textedit.text = textlist.join(' ');
+    // textedit.selection = TextSelection(baseOffset: 2, extentOffset: 10);
+    positionedit.text = (index + 1).toString();
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 17, 35, 49),
       appBar: AppBar(
@@ -32,8 +52,9 @@ class _EditScreenState extends State<EditScreen> {
         actions: [
           TextButton.icon(
             onPressed: () async {
+              // alltext.replaceAll(' █ ', ' ');
               await updateText(
-                textedit.text,
+                alltext,
                 int.parse(positionedit.text) - 1,
               );
               Navigator.pop(context);
@@ -53,20 +74,47 @@ class _EditScreenState extends State<EditScreen> {
         padding: const EdgeInsets.all(20),
         child: Column(
           children: [
-            // TextField(
-            //   controller: headeredit,
-            //   maxLines: 1,
-            //   style: const TextStyle(fontSize: 20, color: Colors.white),
-            // ),
-            TextField(
-              controller: textedit,
-              maxLines: null,
-              style: const TextStyle(fontSize: 17, color: Colors.white),
+            RichText(
+              text: TextSpan(
+                style: const TextStyle(fontSize: 17, color: Colors.white),
+                children: [
+                  TextSpan(
+                    text: alltext.substring(0, untillength),
+                  ),
+                  TextSpan(
+                    text: alltext.substring(untillength, untillength + textlist[index].length),
+                    style: const TextStyle(fontSize: 17, backgroundColor: Color.fromARGB(255, 245, 164, 0), color: Colors.black),
+                  ),
+                  TextSpan(
+                    text: alltext.substring(untillength + textlist[index].length),
+                  )
+                ],
+              ),
             ),
             const SizedBox(
               height: 30,
             ),
-            const Text('позиция в тексте:', style: TextStyle(fontSize: 20, color: Colors.white),),
+            TextField(
+              controller: textedit,
+              maxLines: null,
+              style: const TextStyle(fontSize: 17, color: Colors.white),
+              textInputAction: TextInputAction.done,
+              onEditingComplete: () {
+                setState(() {
+                  // index = int.parse(positionedit.text) - 1;
+                  // untillength = 0;
+                  alltext += textedit.text;
+                  textedit.clear();
+                });
+              },
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            const Text(
+              'позиция в тексте:',
+              style: TextStyle(fontSize: 20, color: Colors.white),
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
@@ -76,9 +124,18 @@ class _EditScreenState extends State<EditScreen> {
                     controller: positionedit,
                     maxLines: 1,
                     style: const TextStyle(fontSize: 20, color: Colors.white),
+                    onEditingComplete: () {
+                      setState(() {
+                        index = int.parse(positionedit.text) - 1;
+                        // untillength = 0;
+                      });
+                    },
                   ),
                 ),
-                const Text('слово', style: TextStyle(fontSize: 20, color: Colors.white),)
+                const Text(
+                  'слово',
+                  style: TextStyle(fontSize: 20, color: Colors.white),
+                ),
               ],
             ),
           ],
